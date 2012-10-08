e5498bffc9123c227612db079b34d7580b819267
Tony Jian <tonytonyjan@gmail.com>
2012-10-08 11:21:21 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twbcl::Application.config.secret_token = '95237fc34cf36bc3d436613a1bc5777df4aa1967283e62fc6fd61e15a0a432a1152008d48cd1287e5926098eafba017fad86f8b1d1d398fbe0e959e7d091dd9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Twbcl::Application.config.session_store :cookie_store, key: '_twbcl_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Twbcl::Application.config.session_store :active_record_store
